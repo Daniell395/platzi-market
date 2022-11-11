f15e61e27a31e94c065eb491214c3b1da9844b99
Daniel Lemos <eda.ni@outlook.com>
2022-11-10 23:59:38 -0500

    Se Añaden los mappers de compra, se ajusta los get-set de Compra

---
+++ src/main/java/com/platzi/platzimarket/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,32 @@
+package com.platzi.platzimarket.persistence.mapper;
+
+import com.platzi.platzimarket.domain.service.Purchase;
+import com.platzi.platzimarket.domain.service.PurchaseItem;
+import com.platzi.platzimarket.persistence.entity.Compra;
+import com.platzi.platzimarket.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad",target = "quantity"),
+            @Mapping(source = "estado",target = "active"),
+    })
+
+    PurchaseItem toPurchaseItem(ComprasProducto comprasProducto);
+    List<PurchaseItem>toPurchasesItems(List<ComprasProducto>comprasProductos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "compra", ignore = true)
+    @Mapping(target = "producto", ignore = true)
+    @Mapping(target = "id.idCompra", ignore = true)
+    ComprasProducto toComprasProducto (PurchaseItem purchaseItem);
+
+
+}
